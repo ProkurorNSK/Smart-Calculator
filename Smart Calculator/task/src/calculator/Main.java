8dbf6b9c6fc9ceae6f0708f0b24558e7c4bf809b
pavel.fedyashin <proka@ngs.ru>
2020-07-31 12:42:56 +0700

    2+2+

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -6,6 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() + scanner.nextInt());
+        while (true) {
+            int a = 0;
+            int b = 0;
+            String text = scanner.nextLine();
+
+            if ("/exit".equals(text)) {
+                System.out.println("Bye!");
+                break;
+            }
+            if (text.isEmpty()) continue;
+
+            String[] numbers = text.split("\\s+");
+            a = Integer.parseInt(numbers[0]);
+            if (numbers.length > 1) {
+                b = Integer.parseInt(numbers[1]);
+            }
+
+            System.out.println(a + b);
+        }
     }
 }
