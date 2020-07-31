4c74e8c83f5f646461539bd39ae287616edc842d
pavel.fedyashin <proka@ngs.ru>
2020-07-31 12:51:06 +0700

    Count them all

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -7,23 +7,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            int a = 0;
-            int b = 0;
             String text = scanner.nextLine();
 
-            if ("/exit".equals(text)) {
-                System.out.println("Bye!");
-                break;
+            switch (text) {
+                case "/exit":
+                    System.out.println("Bye!");
+                    return;
+                case "/help":
+                    System.out.println("The program calculates the sum of numbers");
+                    break;
+                case "":
+                    continue;
+                default:
+                    String[] numbers = text.split("\\s+");
+                    int result = 0;
+                    for (String element: numbers) {
+                        result += Integer.parseInt(element);
+                    }
+                    System.out.println(result);
             }
-            if (text.isEmpty()) continue;
-
-            String[] numbers = text.split("\\s+");
-            a = Integer.parseInt(numbers[0]);
-            if (numbers.length > 1) {
-                b = Integer.parseInt(numbers[1]);
-            }
-
-            System.out.println(a + b);
         }
     }
 }
